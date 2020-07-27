ac429491b3919a7200c7481d0fffc39e71c7985c
Aadish N <aadish.20.1@protosem.tech>
2020-07-27 21:07:17 +0530

    First Change

--- blah.js
+++ blah.js
@@ -1,1 +1,8 @@
 console.log("Hi");
+//git status--> to check if tracked or not
+// git add . --> all files are set to tracked
+// git add "specific file" --> "" specific file
+// git commit -m "message" --> commit to push with Msg
+// git push origin master --> push to master
+// git push origin branch_name --> push to branch
+// git checkout branch_name --> to change branch
